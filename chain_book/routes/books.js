d85473063f2b530bfd98dff3e0dcfec953913989
airontsang <airontsang@yahoo.com>
2017-02-01 21:15:46 +0800

    api文档到账本的添加更改删除

--- chain_book/routes/books.js
+++ chain_book/routes/books.js
@@ -3,8 +3,33 @@ var router = express.Router();
 var Book = require('../proxy/book');
 var jwtauth = require('../selfMiddleware/jwtauth');
 
-router.post('/addInfo*', jwtauth, function (req, res, next) {
-    console.log(res.locals.user)
+router.post('/addBook*', jwtauth, function (req, res, next) {
+    var newBook = {};
+    newBook.founderId = res.locals.user._id;
+    newBook.title = req.query.booksTitle;
+    newBook.place = req.query.booksPlace;
+    newBook.intro = req.query.booksIntro;
+    newBook.picUrl = req.query.booksPic;
+    
+    Book.newAndSave(newBook, function (err, book) {
+        if (err) {
+            res.statusCode = 401;
+            res.json({
+                msg: '后台错误'
+            });
+            return res;
+        } else {
+            res.json({
+                msg: '添加成功'
+            })
+        }
+    })
 });
+
+router.put('/editBookInfo*', jwtauth, function (req, res, next) {
+    var modifiedBook = {};
+
+
+})
 
 module.exports = router;
